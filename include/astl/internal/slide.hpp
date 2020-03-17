0fb71f7099c8e09605819f2d5d9b17515e07332e
Rijad <rijad.rogo@gmail.com>
2020-03-17 23:23:43 +0100

    Cmake changes

--- include/astl/internal/slide.hpp
+++ include/astl/internal/slide.hpp
@@ -12,17 +12,20 @@ namespace astl
 {
 namespace i
 {
-template <typename RandIt>
-// requires RandIt RandomAccessIterator
-auto slide(RandIt first, RandIt last, RandIt pos) -> std::pair<RandIt, RandIt>
-{
-    if (pos < first) return std::make_pair(pos, std::rotate(pos, first, last));
+inline constexpr struct {
+    template <typename RandIt>
+    // requires RandIt RandomAccessIterator
+    auto operator()(RandIt first, RandIt last, RandIt pos) const -> std::pair<RandIt, RandIt>
+    {
+        if (pos < first) return std::make_pair(pos, std::rotate(pos, first, last));
 
-    if (last < pos) return std::make_pair(std::rotate(first, last, pos), pos);
+        if (last < pos) return std::make_pair(std::rotate(first, last, pos), pos);
 
-    return std::make_pair(first, last);
-}
-}// namespace i
-}// namespace astl
+        return std::make_pair(first, last);
+    }
+} slide{};
 
-#endif// ASTL_INCLUDE_SLIDE_HPP
+} // namespace i
+} // namespace astl
+
+#endif // ASTL_INCLUDE_SLIDE_HPP
